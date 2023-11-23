cea1f19ee32f7a117293489fe1fff516945e7a0d
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-23 17:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let  select = document.getElementById("colorSelect");
+let selectOption=document.getElementsByTagName("option");
+let button= document.querySelector("input");
+
+button.addEventListener("click" , ()=>{
+	for (let i = 0; i < selectOption.length; i++) {
+		if(select.value===selectOption[i].value)
+		{
+			selectOption[i].remove();
+			break;
+		}
+		
+	}
+	
+});
